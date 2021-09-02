35911979afa6e8c9189a31ede87e4e8c2e81a7f6
Bindu <binduvukyam@gmail.com>
2021-09-01 19:31:58 -0700

    Fixed few cases in bidding dao

--- biddingApi/biddingApi/src/test/java/biddingApi/biddingApi/TestBiddingDao.java
+++ biddingApi/biddingApi/src/test/java/biddingApi/biddingApi/TestBiddingDao.java
@@ -1,9 +1,13 @@
 package biddingApi.biddingApi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,13 +17,27 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import biddingApi.biddingApi.Dao.BiddingDao;
 import biddingApi.biddingApi.Entities.BiddingData;
 import biddingApi.biddingApi.ErrorConstants.Constants;
 
+
 @DataJpaTest
 public class TestBiddingDao {
+	
+	public static void wait(int ms)
+	{
+	    try
+	    {
+	        Thread.sleep(ms);
+	    }
+	    catch(InterruptedException ex)
+	    {
+	        Thread.currentThread().interrupt();
+	    }
+	}
 
 	@Autowired
 	private TestEntityManager entityManager;
@@ -51,14 +69,13 @@ public class TestBiddingDao {
 		BiddingData savedInDb4 = entityManager.persist(listBiddingData.get(4));
 		BiddingData savedInDb5 = entityManager.persist(listBiddingData.get(5));
 
-		Iterable<BiddingData> allBids = biddingDao.findByLoadId(Constants.LOAD_ID);
-		List<BiddingData> list = new ArrayList<>();
-
-		for (BiddingData t : allBids) {
-			list.add(t);
+		List<BiddingData> allBids = biddingDao.findByLoadId(Constants.LOAD_ID);
+		
+		assertThat(allBids.size()).isEqualTo(4);
+		
+		for(BiddingData data : allBids) {
+			assertTrue(listBiddingData.contains(data));
 		}
-		assertThat(list.size()).isEqualTo(4);
-
 	}
 
 	@Test
@@ -76,62 +93,67 @@ public class TestBiddingDao {
 
 		currentPage = PageRequest.of(0, (int) Constants.pageSize);
 
-//		Iterable<BiddingData> allBids = biddingDao.findByLoadId("load:123", currentPage);
-//		List<BiddingData> list = new ArrayList<>();
-	
-//		for (BiddingData t : allBids) {
-//			list.add(t);
-//		}
-		List<BiddingData> list = biddingDao.findByLoadId("load:123", currentPage);
-		assertThat(list.size()).isEqualTo(4);
+		List<BiddingData> allBids = biddingDao.findByLoadId("load:123", currentPage);
+
+		assertThat(allBids.size()).isEqualTo(4);
+		for(BiddingData data : allBids) {
+			assertTrue(listBiddingData.contains(data));
+		}
 
 		Pageable nextPage = PageRequest.of(1, (int) Constants.pageSize);
-//		Iterable<BiddingData> allBidsNextPage = biddingDao.findByLoadId("load:123", nextPage);
+		List<BiddingData> allBidsNextPage = biddingDao.findByLoadId("load:123", nextPage);
 
-//		List<BiddingData> listNextPage = new ArrayList<>();
-
-//		for (BiddingData t : allBidsNextPage) {
-//			listNextPage.add(t);
-//		}
-		List<BiddingData> listNextPage = biddingDao.findByLoadId("load:123", nextPage);
-		assertThat(listNextPage.size()).isEqualTo(0);
+		assertThat(allBidsNextPage.size()).isEqualTo(0);
+		for(BiddingData data : allBidsNextPage) {
+			assertTrue(listBiddingData.contains(data));
+		}
 	}
 
 	@Test
 	public void testFindByTransporterIdWithPagination() {
 
-		Pageable currentPage;
-		List<BiddingData> listBiddingData = createBiddingData();
-
-		BiddingData savedInDb = entityManager.persist(listBiddingData.get(0));
-		BiddingData savedInDb1 = entityManager.persist(listBiddingData.get(1));
-		BiddingData savedInDb2 = entityManager.persist(listBiddingData.get(2));
-		BiddingData savedInDb3 = entityManager.persist(listBiddingData.get(3));
-		BiddingData savedInDb4 = entityManager.persist(listBiddingData.get(4));
-		BiddingData savedInDb5 = entityManager.persist(listBiddingData.get(5));
-
-		currentPage = PageRequest.of(0, (int) Constants.pageSize);
-		Iterable<BiddingData> allBids = biddingDao.findByTransporterId(Constants.TRANSPORTER_ID, currentPage);
-
-		List<BiddingData> list = new ArrayList<>();
-
-		for (BiddingData t : allBids) {
-			list.add(t);
+		List<BiddingData> transporterId1 = new ArrayList<BiddingData>();
+		List<BiddingData> transporterId2 = new ArrayList<BiddingData>();
+		for(int i=11; i<=28; i++)
+		{
+			boolean firstTransporter;
+			if (i%2 == 1) {
+				firstTransporter = true;
+			}else {
+				firstTransporter = false;
+			}
+			
+			String transporterId = firstTransporter ? "tr:1" : "tr:2";
+			
+			BiddingData savedInDb = entityManager.persist(new BiddingData("bid:"+i, transporterId, "load:"+i, (long) 20,
+					null, BiddingData.Unit.PER_TON, Arrays.asList("truck:123"), false, true, null, Timestamp.valueOf("2021-07-28 23:28:50.134")));
+			
+			entityManager.flush();
+			
+			if(firstTransporter) transporterId1.add(savedInDb);
+			else  transporterId2.add(savedInDb);
+			
+			wait(1);
 		}
-		assertThat(list.size()).isEqualTo(2);
-
-		Pageable nextPage = PageRequest.of(1, (int) Constants.pageSize);
-		Iterable<BiddingData> allBidsNextPage = biddingDao.findByTransporterId(Constants.TRANSPORTER_ID, nextPage);
-
-		List<BiddingData> listNextPage = new ArrayList<>();
-
-		for (BiddingData t : allBidsNextPage) {
-			listNextPage.add(t);
-		}
-		assertThat(listNextPage.size()).isEqualTo(0);
-
+		
+		Collections.reverse(transporterId1);
+		Collections.reverse(transporterId2);
+		
+		PageRequest firstPage = PageRequest.of(0, 5, Sort.Direction.DESC, "timestamp"),
+		            secondPage = PageRequest.of(1, 5, Sort.Direction.DESC, "timestamp"),
+		            thirdPage = PageRequest.of(2, 5, Sort.Direction.DESC, "timestamp");
+		assertThat(transporterId1.subList(0, 5)).isEqualTo(biddingDao.findByTransporterId("tr:1",firstPage));
+	    assertThat(transporterId1.subList(5, 9)).isEqualTo(biddingDao.findByTransporterId("tr:1",secondPage));
+	    assertThat(transporterId1.subList(9, 9)).isEqualTo(biddingDao.findByTransporterId("tr:1",thirdPage));
+	    
+	    assertThat(transporterId2.subList(0, 5)).isEqualTo(biddingDao.findByTransporterId("tr:2",firstPage));
+	    assertThat(transporterId2.subList(5, 9)).isEqualTo(biddingDao.findByTransporterId("tr:2",secondPage));
+	    assertThat(transporterId2.subList(9, 9)).isEqualTo(biddingDao.findByTransporterId("tr:2",thirdPage));
+	
 	}
+	
 
+	//there will be only one record for tranporterid and loadid combination
 	@Test
 	public void testFindByLoadIdAndTransporterIdWithPagination() {
 
@@ -143,45 +165,44 @@ public class TestBiddingDao {
 		BiddingData savedInDb2 = entityManager.persist(listBiddingData.get(2));
 
 		currentPage = PageRequest.of(0, (int) Constants.pageSize);
-		Iterable<BiddingData> allBids = biddingDao.findByLoadIdAndTransporterId(Constants.LOAD_ID,
+		List<BiddingData> allBids = biddingDao.findByLoadIdAndTransporterId(Constants.LOAD_ID,
 				Constants.TRANSPORTER_ID, currentPage);
 
-		List<BiddingData> list = new ArrayList<>();
+		assertThat(allBids.size()).isEqualTo(1);
+		assertEquals(savedInDb1,allBids.get(0));
+	}
+	
+	@Test
+	public void testFindByLoadIdAndTransporterIdWithOutPagination() {
 
-		for (BiddingData t : allBids) {
-			list.add(t);
-		}
-		assertThat(list.size()).isEqualTo(1);
+		List<BiddingData> listBiddingData = createBiddingData();
 
+		BiddingData savedInDb = entityManager.persist(listBiddingData.get(0));
+		BiddingData savedInDb1 = entityManager.persist(listBiddingData.get(1));
+		BiddingData savedInDb2 = entityManager.persist(listBiddingData.get(2));
+
+		BiddingData fromDao = biddingDao.findByLoadIdAndTransporterId(Constants.LOAD_ID,
+				Constants.TRANSPORTER_ID);
+		assertEquals(savedInDb1,fromDao);
+		
 	}
 
 	@Test
 	public void testUpdate() {
 
 		List<BiddingData> listBiddingData = createBiddingData();
-
 		BiddingData savedInDb = entityManager.persist(listBiddingData.get(0));
-
-		// entityManager.persist(truckData);
-		Optional<BiddingData> getFromDb = biddingDao.findById(Constants.ID);
-
-		listBiddingData.get(0).setCurrentBid((long) 100);
-
+		
+		listBiddingData.get(0).setCurrentBid((long) 101);
+		
 		entityManager.persist(listBiddingData.get(0));
-
-		Iterable<BiddingData> allBids = biddingDao.findAll();
-
-		List<BiddingData> list = new ArrayList<>();
-
-		for (BiddingData t : allBids) {
-			list.add(t);
-		}
-		assertThat(list.size()).isEqualTo(1);
-
-		assertThat(list.get(0).getCurrentBid()).isEqualTo(100);
-
+		
+		Optional<BiddingData> getFromDb = biddingDao.findById(Constants.ID);
+		
+		assertEquals(101,getFromDb.get().getCurrentBid());
+	
 	}
-
+	
 	@Test
 	public void testDelete() {
 
@@ -192,13 +213,12 @@ public class TestBiddingDao {
 
 		entityManager.remove(savedInDb1);
 
-		Iterable<BiddingData> allBids = biddingDao.findAll();
-		List<BiddingData> list = new ArrayList<>();
+		List<BiddingData> allBids = biddingDao.findAll();
 
-		for (BiddingData t : allBids) {
-			list.add(t);
+		assertThat(allBids.size()).isEqualTo(1);
+		for (BiddingData data : allBids) {
+			assertTrue(listBiddingData.contains(data));
 		}
-		assertThat(list.size()).isEqualTo(1);
 
 	}
 
